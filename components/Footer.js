848861e65614fd0b269a7a482a17c5ec9c8fc0f5
LukeJessop <jessop.lucas@gmail.com>
2023-11-03 02:53:36 -0600

    did a lot of work for a first commit

---
+++ components/Footer.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+import Link from "next/link"
+
+function Footer() {
+  return (
+    <footer className="flex justify-between items-center p-4 bg-zinc-100 dark:bg-zinc-900 text-zinc-500 dark:text-zinc-400 w-full">
+      <div>© 2023 Cocktail Recipe Finder</div>
+      <div className="flex gap-2">
+        <Link href="#">
+          <svg
+            className=" h-6 w-6"
+            fill="none"
+            height="24"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+          </svg>
+        </Link>
+        <Link href="#">
+          <svg
+            className=" h-6 w-6"
+            fill="none"
+            height="24"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+          </svg>
+        </Link>
+        <Link href="#">
+          <svg
+            className=" h-6 w-6"
+            fill="none"
+            height="24"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect height="20" rx="5" ry="5" width="20" x="2" y="2" />
+            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+            <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+          </svg>
+        </Link>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
